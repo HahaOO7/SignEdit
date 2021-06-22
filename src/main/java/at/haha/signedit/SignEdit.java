4fcf0c1e60d9ea5fd0d74725e5eb63f3423277fa
Haha007 <nichtechtanonym@gmail.com>
2021-06-22 11:24:57 +0200

    lombok, 1.17, formatting

--- src/main/java/at/haha/signedit/SignEdit.java
+++ src/main/java/at/haha/signedit/SignEdit.java
@@ -6,8 +6,8 @@ import java.util.Objects;
 
 public final class SignEdit extends JavaPlugin {
 
-	@Override
-	public void onEnable() {
-		Objects.requireNonNull(getCommand("sign")).setExecutor(new SignCommand(this));
-	}
+    @Override
+    public void onEnable() {
+        Objects.requireNonNull(getCommand("sign")).setExecutor(new SignCommand(this));
+    }
 }
